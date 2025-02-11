9de3aaf93d071c284ae8e94d382c41ec8273c035
bissembayevd <bissembayevd@list.ru>
2025-02-11 11:52:19 +0500

    lab3

---
+++ lab3/to-do-list/to-do-list.js
@@ -0,0 +1,30 @@
+const inputBox = document.getElementById("todo-input");
+const listContainer = document.getElementById("todo-list");
+function addTask(){
+    if (inputBox.value === ''){
+        alert("Error empty task!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+}
+listContainer.addEventListener("click", function (e){
+    if (e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+    }
+    else if (e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+    }
+},false);
+
+document.getElementById('todo-input').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        document.getElementById('todo-button').click(); // Симулируем клик по кнопке
+    }
+});
